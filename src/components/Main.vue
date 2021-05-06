4041ef429bc72809babad0febe0e3332c3364f23
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-06 17:32:04 +0200

    axios

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,39 @@
 <template>
   <main>
-    <div>
-      <h1>ciao</h1>
+    <div class="album">
+      <h1>ok</h1>
     </div>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Main",
+  data() {
+    return {
+      apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
+      discoList: [],
+      loading: true,
+    };
+  },
+  created() {
+    this.getDisco();
+  },
+  methods: {
+    getDisco() {
+      axios;
+    },
+  },
 };
 </script>
+
+
 
 <style>
 main {
   width: 100%;
   height: 651px;
-  background-color: #1f263beb;
+  background-color: #151a28e3;
 }
 </style>
