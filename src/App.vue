68a84b41fe7c9e1f7debe49004de1921e02225c3
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-06 15:40:43 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <div>
+      <img src="../assets/logo.png" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style  scoped>
+div {
+  height: 70px;
+  width: 100%;
+  background-color: #222326;
+  display: flex;
+  align-content: flex-start;
+  align-items: center;
+}
+img {
+  height: 60px;
+  margin: 10px;
+}
+</style>
+
